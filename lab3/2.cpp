dff47ce262e16b21ce69ac8868b2708ad0460e44
Vasilkova Polina <pvasilkova@yandex.ru>
2015-10-28 00:15:06 +0300

    lab3_punct3 1) добавлено перечисление emum Error 2) исправлено повторение выражений в switch 3) функция convert возвращает "код ошибки", результат считается отдельно

--- lab3/2.cpp
+++ lab3/2.cpp
@@ -1,59 +1,64 @@
 #include "sdt.h"
-int convert(double t, char from, char to)
-{   double K,t1;
-    if (to==from) return t;
+enum Error {     //перечисление для ошибок
+	NoError,    //Нет ошибок
+	ErrTemp,   //Ошибка в значении температуры (<0K)
+	ErrScale  //Неизвестная шкала from или to
+};
+Error convert(double t, char from, char to, double& result)
+{   double K;
+    if (to==from) result=t;
     switch (from)
     {
-    case 'k': K=t;
-              break;
+    case 'k':
     case 'K': K=t;
               break;
-    case 'C': K=t+273.15;
-              break;
+    case 'C':
     case 'c': K=t+273.15;
               break;
-    case 'F': K=(5*(t-32))/9+273.15;
-              break;
+    case 'F':
     case 'f': K=(5*(t-32))/9+273.15;
               break;
-    default: return 1;
+    default: return ErrScale;
     }
-    if (K<0) return 2;
-
-    if (K>=0)
+    if (K<0) return ErrTemp;
+    else if (K>=0)
     {  switch (to)
       {
-        case 'C': return (K-273.15); break;
-        case 'c': return (K-273.15); break;
+        case 'C':
+        case 'c': result=(K-273.15); return NoError; break;
 
-        case 'F': return ((9*(K-273.15))/5+32); break;
-        case 'f': return ((9*(K-273.15))/5+32); break;
+        case 'F':
+        case 'f': result=((9*(K-273.15))/5+32); return NoError; break;
 
-        case 'K': return K; break;
-        case 'k': return K; break;
-        default: return 1;
+        case 'K':
+        case 'k': result=K; return NoError; break;
+        default:  return ErrScale;
       }
     }
 }
 
 int main()
 {
-    double t;
+    double t,K,C,F;
     char s;
     vector<double> temp;
     vector<char> name;
     cout << "Input the temperature: 10F-Fahrenheit,10C-Celsius,10K-Kelvin: ";
     while (cin>>t>>s)
     {
-        switch (convert(t,s,'K'))
-        { case 1: cout <<"Incorrect nomination of scale! Try again! \n";
+        switch (convert(t,s,'K',K))
+        { case ErrScale: cout <<"Incorrect nomination of scale! Try again! \n";
                   break;
-          case 2: cout <<"Too low temperature!\n";
+          case ErrTemp: cout <<"Too low temperature!\n";
                   break;
-          default:
-            temp.push_back(convert(t,s,'C'));
-            temp.push_back(convert(t,s,'K'));
-            temp.push_back(convert(t,s,'F'));
+          case NoError:
+            convert(t,s,'C',C);
+            temp.push_back(C);
+            temp.push_back(K);
+            convert(t,s,'F',F);
+            temp.push_back(F);
+            break;
+
         }
       cout << "Input the temperature: 10F-Fahrenheit,10C-Celsius,10K-Kelvin: ";
     }
@@ -63,5 +68,4 @@ int main()
         printf("%4.2f\t", temp[i]);
         if ((i+1)%3==0) cout <<"\n";
     }
-    return 0;
 }
